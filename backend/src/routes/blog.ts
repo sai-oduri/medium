1cea09e38acce532e114339e88dac8e34e61e0ee
sai-oduri <saioduri95@gmail.com>
2024-09-26 23:45:13 +0530

    backend api modify

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -111,7 +111,19 @@ blogRouter.get('/bulk', async (c) => {
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
 
-    const blogs = await prisma.post.findMany();
+    const blogs = await prisma.post.findMany({
+        select: {
+            content: true,
+            title: true,
+            id: true,
+            author: {
+                select: {
+                    name: true,
+                }
+            }
+
+        }
+    });
 
     return c.json({
         blogs
@@ -131,6 +143,16 @@ blogRouter.get('/:id', async (c) => {
         const blog = await prisma.post.findFirst({
             where: {
                 id
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        name: true,
+                    }
+                }
             }
         })
 
